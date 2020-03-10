7168ec687ca394c474e20604f3d1027f1a773248
Rene Nyffenegger <mail@renenyffenegger.ch>
2020-03-11 00:29:13 +0100

    Create createRepository.ps1

---
+++ V4-graphql/createRepository.ps1
@@ -0,0 +1,15 @@
+#  $secString=convertTo-secureString $env:githubAPItoken -asPlainText -force
+
+$response = invoke-webrequest `
+    https://api.github.com/graphql            `
+   -method         POST                       `
+   -contentType    application/json           `
+   -headers @{Authorization = "Bearer $env:githubAPItoken"} `
+   -body '{ "query":
+            "mutation { createRepository ( input: { name: \"the-foo-repo\", visibility: PRIVATE } ) { clientMutationId } } " 
+          }'
+          
+#
+#  Use -authentication in PowerShell 6 or 7 ?
+#   -authentication bearer -token $secString   `
+#          
