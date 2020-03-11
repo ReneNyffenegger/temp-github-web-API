73d20a89d37e014389785dc019d198c135d39371
Rene Nyffenegger <mail@renenyffenegger.ch>
2020-03-11 01:16:17 +0100

    upload

--- V3/overwrite-file.ps1
+++ V3/overwrite-file.ps1
@@ -1,16 +1,22 @@
-$secString=convertTo-secureString $env:githubAPItoken -asPlainText -force
-# invoke-webrequest https://api.github.com/repos/ReneNyffenegger/ldp-Ordnung -authentication bearer -token $secString
-# invoke-webrequest https://api.github.com/repos/ReneNyffenegger/ldp-Ordnung -headers @{Authorization = "Bearer $env:githubAPItoken"}
-curl.exe -u x:$env:githubAPItoken  https://api.github.com/repos/ReneNyffenegger/$repo
+$fileName = 'overwrite-file.ps1'
+$fileToUpload = "$pwd\$fileName"
 
-
-$fileToUpload = "$pwd\upload-sources.ps1"
-# $fileToUpload = "$pwd\classes.ps1"
 $base64 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($fileToUpload))
-# curl.exe -u x:$env:githubAPItoken --upload-file upload-sources.ps1 https://api.github.com/ReneNyffenegger/repos/ReneNyffenegger/temp-PowerShell/misc/sqlite3/upload-sources.ps1
-$txt = '{{"message": "upload",  "committer": {{"name": "nm", "email": "email@abfall.ch"}}, "content": "{0}" }}' -f $base64
-# $txt
-curl.exe -i -X PUT -u x:$env:githubAPItoken -d "$txt" https://api.github.com/repos/ReneNyffenegger/temp-PowerShell/misc/sqlite3/classes.ps1
-# curl.exe -i -X PUT -u x:$env:githubAPItoken -d "$txt" https://api.github.com/repos/ReneNyffenegger/temp-PowerShell/misc/sqlite3/classes.ps1
 
-# curl.exe https://api.github.com/hub
+$repo='temp-github-web-API'
+$pathInRepo='V3'
+
+$response = invoke-restMethod `
+    https://api.github.com/repos/ReneNyffenegger/$repo/contents/$pathInRepo/$fileName            `
+   -headers @{Authorization = "Bearer $env:githubAPItoken"}
+
+$sha = $response.sha
+
+$body = '{{"message": "upload", "content": "{0}", "sha": "{1}" }}' -f $base64, $sha
+
+invoke-webrequest `
+    https://api.github.com/repos/ReneNyffenegger/$repo/contents/$pathInRepo/$fileName            `
+   -method         PUT                        `
+   -contentType    application/json           `
+   -headers @{Authorization = "Bearer $env:githubAPItoken"} `
+   -body $body
